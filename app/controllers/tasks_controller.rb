5d26431ffd2c0d7952b95868b06db057840e3c86
Alex Robinson <alexandra.scott335@gmail.com>
2015-05-06 05:07:34 -0600

    can change the status of a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,17 +5,29 @@ class TasksController < ApplicationController
 
   def create
     list = List.find(params[:task][:list_id])
-    task = list.tasks.new(task_params)
-    if task.save
-      flash[:notice] = "Task successfully created"
-      redirect_to list_path(list.id)
+    @task = list.tasks.new(task_params)
+    if @task.save
+      respond_to do |format|
+        format.html { flash[:notice] = "Task successfully created"; redirect_to list_path(list.id) }
+        format.js
+      end
     else
       flash[:error] = "Something went wrong, please try again"
       render :new
     end
   end
 
-  def udpate
+  def edit
+    @task = Task.find(params[:id])
+    @list_id = params[:list_id]
+  end
+
+  def update
+    Task.update(params[:id], task_params)
+    respond_to do |format|
+      format.html { redirect_to list_path(params[:task][:list_id]) }
+      format.js
+    end
   end
 
   def destroy
