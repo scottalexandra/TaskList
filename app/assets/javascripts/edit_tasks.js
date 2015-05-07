f5918ec2ce4d488660a62bf677fd445762a33904
Alex Robinson <alexandra.scott335@gmail.com>
2015-05-07 07:19:28 -0600

    not currently working, can't find the correct selector

--- app/assets/javascripts/edit_tasks.js
+++ app/assets/javascripts/edit_tasks.js
@@ -3,15 +3,26 @@ $(function(){
     var taskId = $(this).closest('tr').data('task-id');
     var listID = $(this).closest('tr').data('list-id');
     var taskElement = $(this).closest("tr");
-    $.ajax({
-      url: '/tasks/' + taskId,
-      type: 'PUT',
-      data: {task: {completed: true, }},
-      success: function(data) {
-        console.log("completed complete");
-        taskElement.appendTo(".complete-tasks");
-        console.log(data);
-      }
-    })
+    if($(this).closest('div') == "<div class='incomplete-task'></div>" ) {
+      $.ajax({
+        url: '/tasks/' + taskId,
+        type: 'PUT',
+        data: { task: { completed: true } },
+        success: function(data) {
+          taskElement.appendTo(".complete-tasks");
+          console.log(data);
+        }
+      });
+    } else if($(this).closest('div') == '.complete-task') {
+      $.ajax({
+        url: '/tasks/' + taskId,
+        type: 'PUT',
+        data: { task: { completed: false } },
+        success: function(data) {
+          taskElement.appendTo(".incomplete-tasks");
+          console.log(data);
+        }
+      });
+    };
   });
 });
