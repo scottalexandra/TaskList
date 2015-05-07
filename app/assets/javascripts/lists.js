84f2182ffc801897dcda2d0f35304fed589e793d
Alex Robinson <alexandra.scott335@gmail.com>
2015-05-07 11:11:13 -0600

    the filtering works

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -18,4 +18,15 @@ $(document).ready(function(){
       $(buttonID2).removeClass("active");
     });
   }
+
+  $.expr[":"].containsNoCase = function(el, i, m) {
+      var search = m[3];
+      if (!search) return false;
+      return new RegExp(search, "i").test($(el).text());
+    };
+  $("#filter").keyup(function() {
+    var search = $(this).val();
+    $("tbody").show();
+    if (search) $("tbody").not(":containsNoCase(" + search + ")").hide();
+  });
 })
